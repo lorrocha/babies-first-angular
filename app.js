4ff22119555cd20a757a33a1c236b42e4e0a1fe5
Lorrayne Rocha <lrocha@fiksu.com>
2015-01-01 18:03:41 -0500

    Ugh, javascript and it's brackets everywhere. Fixin that.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 angular.module('tumblrApp', []).controller('MainCtrl', ['$scope',
   function($scope){
-    $scope.test = 'Hello world!';}
+    $scope.test = 'Hello world!';
     
     $scope.blogs = [
     'sailorsunspot',
     'moonlight1130',
     'crunchbuttsteak',
     'docholigay',
-    'rocketonthemoon']
+    'rocketonthemoon'];
+  }
 ]);
