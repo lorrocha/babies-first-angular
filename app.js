a94becf8bf73083b00343dec7a8ed8020172b90f
Lorrayne Rocha <lrocha@fiksu.com>
2015-01-01 17:59:30 -0500

    So. Good job. I understand the two-way binding in angular a lot more now. Buuuut no idea how to turn on the server. This is barebones in here. Will have to look back at the tutorial to be like wtf soon.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 angular.module('tumblrApp', []).controller('MainCtrl', ['$scope',
   function($scope){
     $scope.test = 'Hello world!';}
+    
     $scope.blogs = [
     'sailorsunspot',
     'moonlight1130',
