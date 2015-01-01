4d964a29faaeda996cf4bd4a70ce32d68c318237
Lorrayne Rocha <lrocha@fiksu.com>
2015-01-01 17:53:34 -0500

    Theoretically, adding a few scope elements to tests this.

---
+++ app.js
@@ -0,0 +1,10 @@
+angular.module('tumblrApp', []).controller('MainCtrl', ['$scope',
+  function($scope){
+    $scope.test = 'Hello world!';}
+    $scope.blogs = [
+    'sailorsunspot',
+    'moonlight1130',
+    'crunchbuttsteak',
+    'docholigay',
+    'rocketonthemoon']
+]);
